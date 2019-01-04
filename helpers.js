375bcfe7ae2245714c798e366950808858f6fca4
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-01-04 08:53:40 -0500

    Used helpers in pug

--- helpers.js
+++ helpers.js
@@ -19,7 +19,7 @@ exports.staticMap = ([lng, lat]) => `https://maps.googleapis.com/maps/api/static
 exports.icon = (name) => fs.readFileSync(`./public/images/icons/${name}.svg`);
 
 // Some details about the site
-exports.siteName = `Now That's Delicious!`;
+exports.siteName = `Thrift Spending Tracker`;
 
 exports.menu = [
   { slug: '/stores', title: 'Stores', icon: 'store', },
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.render('hello');
+  res.render('hello', {
+    title: 'Home'
+  });
 });
 
 module.exports = router;
