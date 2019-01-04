0db1ec1d4a589a0c863cabca64125999bc527d16
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-01-04 08:32:21 -0500

    Set up first pug file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-// Do work here
 router.get('/', (req, res) => {
-  res.send('Hey! It works!');
+  res.render('hello');
 });
 
 module.exports = router;
