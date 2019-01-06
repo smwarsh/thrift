13e79375ff0aca9c936ba27743d43cb23d16799f
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-01-06 11:46:46 -0500

    Set up to add transactions

--- controllers/transactionController.js
+++ controllers/transactionController.js
@@ -1,3 +1,12 @@
 exports.homePage = (req, res) => {
   res.render('index');
 }
+
+exports.addTransaction = (req, res) => {
+  res.render('editTransaction', { title: 'Add Transaction' })
+}
+
+exports.createTransaction = (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+}
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const transactionController = require('../controllers/transactionController')
 
 router.get('/', transactionController.homePage);
+router.get('/add', transactionController.addTransaction);
+router.post('/add', transactionController.createTransaction);
 
 module.exports = router;
