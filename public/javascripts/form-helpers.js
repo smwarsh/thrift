8f039bf4bcfca83b63c8928e9aca060be8aa88f7
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-01-19 18:17:57 -0500

    Made form dynamic based on radio buttons

--- public/javascripts/form-helpers.js
+++ public/javascripts/form-helpers.js
@@ -4,11 +4,12 @@ function populate(select1, select2) {
   let s1 = document.getElementById(select1);
   let s2 = document.getElementById(select2);
   s2.innerHTML = ""; // clear out whatever is in it already
-  if(s1.value === 'Income') {
-    var optionArray = ['|Select a Category', 'Income - Salary|Salary', 'Income - Cash Back|Cash Back', 'Income - Gifts|Gifts', 'Income - Other|Other'];
-  } else if(s1.value === 'Expense') {
+  if(s1.value === 'expense') {
     var optionArray = ['|Select a Category', 'Expense - Food|Food', 'Expense - Transportation|Transportation', 'Expense - Trips|Trips', 'Expense - Gifts|Gifts', 'Expense - Health|Health', 'Expense - Beauty|Beauty', 'Expense - Recreational Activities|Recreational Activities', 'Expense - Shopping|Shopping', 'Expense - Sports|Sports', 'Expense - Pets|Pets', 'Expense - Education|Education', 'Expense - Entertainment|Entertainment', 'Expense - Work|Work', 'Expense - Housing|Housing', 'Expense - Other|Other'];
-  } else {
+  } else if(s1.value === 'income') {
+    var optionArray = ['|Select a Category', 'Income - Salary|Salary', 'Income - Cash Back|Cash Back', 'Income - Gifts|Gifts', 'Income - Other|Other'];
+  }
+  else {
     var optionArray = ['|Select a Category'];
   }
 
