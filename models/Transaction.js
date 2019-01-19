d9ad812db57370ff0be640057e57e03254bff903
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-01-18 22:22:46 -0500

    Fixed inaccurate decimal math in Transaction price setter

--- models/Transaction.js
+++ models/Transaction.js
@@ -3,13 +3,13 @@ mongoose.Promise = global.Promise;
 const slug = require('slugs'); // I don't think I need this for Thrift
 
 function toInteger(p) {
-  return parseInt((p * 100));
+  return Math.round((p * 100));
 }
 
 function toPrice(value) {
   // return (value / 100).toFixed(2);
   value /= 100;
-  return parseFloat(Math.round(value + "e" + 2) + "e-" + 2).toFixed(2)
+  return parseFloat(Math.round(value + "e" + 2) + "e-" + 2).toFixed(2);
   // I'll be able to test this soon, when I find the db entry
   // to edit the entry, later in Module 4
 }
