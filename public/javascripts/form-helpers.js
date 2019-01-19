de370ea26d033191ffa74869b618be8d3c709435
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-01-19 18:36:25 -0500

    Refactored to use forEach instead of for loop and 2-D array instead of split()

--- public/javascripts/form-helpers.js
+++ public/javascripts/form-helpers.js
@@ -5,19 +5,18 @@ function populate(select1, select2) {
   let s2 = document.getElementById(select2);
   s2.innerHTML = ""; // clear out whatever is in it already
   if(s1.value === 'expense') {
-    var optionArray = ['|Select a Category', 'Expense - Food|Food', 'Expense - Transportation|Transportation', 'Expense - Trips|Trips', 'Expense - Gifts|Gifts', 'Expense - Health|Health', 'Expense - Beauty|Beauty', 'Expense - Recreational Activities|Recreational Activities', 'Expense - Shopping|Shopping', 'Expense - Sports|Sports', 'Expense - Pets|Pets', 'Expense - Education|Education', 'Expense - Entertainment|Entertainment', 'Expense - Work|Work', 'Expense - Housing|Housing', 'Expense - Other|Other'];
+    var optionArray = [['','Select a Category'], ['Expense - Food','Food'], ['Expense - Transportation','Transportation'], ['Expense - Trips','Trips'], ['Expense - Gifts','Gifts', 'Expense - Health','Health'], ['Expense - Beauty','Beauty'], ['Expense - Recreational Activities','Recreational Activities'], ['Expense - Shopping','Shopping', 'Expense - Sports','Sports', 'Expense - Pets','Pets'], ['Expense - Education','Education'], ['Expense - Entertainment','Entertainment'], ['Expense - Work','Work'], ['Expense - Housing','Housing'], ['Expense - Other','Other']];
   } else if(s1.value === 'income') {
-    var optionArray = ['|Select a Category', 'Income - Salary|Salary', 'Income - Cash Back|Cash Back', 'Income - Gifts|Gifts', 'Income - Other|Other'];
+    var optionArray = [['','Select a Category'], ['Income - Salary','Salary'], ['Income - Cash Back','Cash Back'], ['Income - Gifts','Gifts'], ['Income - Other','Other']];
   }
   else {
-    var optionArray = ['|Select a Category'];
+    var optionArray = [['','Select a Category']];
   }
 
-  for(let option in optionArray) {
-    let pair = optionArray[option].split('|');
+  optionArray.forEach(function(option) {
     var newOption = document.createElement('option');
-    newOption.value = pair[0];
-    newOption.innerHTML = pair[1];
+    newOption.value = option[0];
+    newOption.innerHTML = option[1];
     s2.options.add(newOption);
-  }
+  });
 }
