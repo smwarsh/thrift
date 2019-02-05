ef6ee3b9aa90f854137d797e504711d0513a868f
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-02-04 23:25:43 -0500

    Displayed transactions under their category heading

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -1,1 +1,8 @@
 document.write('Hey');
+
+function getTransactions(transactions, category) {
+  const correctCategory = transactions.find( object =>
+    object._id.category === category
+  );
+  return correctCategory.transactions;
+}
