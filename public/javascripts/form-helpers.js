b2130f3bcf3a6a169c4055f1d3788eceb9ff9db4
Steph Warsh <15115661+smwarsh@users.noreply.github.com>
2019-02-15 23:01:54 -0500

    Fixed another typo in dropdown

--- public/javascripts/form-helpers.js
+++ public/javascripts/form-helpers.js
@@ -5,7 +5,7 @@ function populate(select1, select2) {
   let s2 = document.getElementById(select2);
   s2.innerHTML = ""; // clear out whatever is in it already
   if(s1.value === 'expense') {
-    var optionArray = [['','Select a Category'], ['Expense - Food','Food'], ['Expense - Transportation','Transportation'], ['Expense - Trips','Trips'], ['Expense - Gifts','Gifts', 'Expense - Health','Health'], ['Expense - Beauty','Beauty'], ['Expense - Recreational Activities','Recreational Activities'], ['Expense - Shopping','Shopping'], ['Expense - Sports','Sports'], ['Expense - Pets','Pets'], ['Expense - Education','Education'], ['Expense - Entertainment','Entertainment'], ['Expense - Work','Work'], ['Expense - Housing','Housing'], ['Expense - Other','Other']];
+    var optionArray = [['','Select a Category'], ['Expense - Food','Food'], ['Expense - Transportation','Transportation'], ['Expense - Trips','Trips'], ['Expense - Gifts','Gifts'], ['Expense - Health','Health'], ['Expense - Beauty','Beauty'], ['Expense - Recreational Activities','Recreational Activities'], ['Expense - Shopping','Shopping'], ['Expense - Sports','Sports'], ['Expense - Pets','Pets'], ['Expense - Education','Education'], ['Expense - Entertainment','Entertainment'], ['Expense - Work','Work'], ['Expense - Housing','Housing'], ['Expense - Other','Other']];
   } else if(s1.value === 'income') {
     var optionArray = [['','Select a Category'], ['Income - Salary','Salary'], ['Income - Cash Back','Cash Back'], ['Income - Gifts','Gifts'], ['Income - Other','Other']];
   }
